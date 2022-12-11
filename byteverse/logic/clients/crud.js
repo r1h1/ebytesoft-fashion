1adbaa36c9723ba3dc00b7ef26adaf65139ff433
Daniel Rivas <de.rivasherrera@gmail.com>
2022-12-10 20:57:39 -0600

    v1.8 - Se rediseñaron los módulos

---
+++ byteverse/logic/clients/crud.js
@@ -0,0 +1,22 @@
+function apiDelete() {
+
+    Swal.fire({
+        title: '¿Estás Seguro?',
+        text: "No puedes revertir esta acción",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        cancelButtonText: 'Cancelar',
+        confirmButtonText: 'Continuar'
+    }).then((result) => {
+        if (result.isConfirmed) {
+            Swal.fire({
+                title: 'Operación Exitosa',
+                icon: 'success',
+                confirmButtonText: 'Entendido'
+            })
+        }
+    });
+
+}
