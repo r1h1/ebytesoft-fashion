9e65fb9f6bd73d1051aaccef1ad0744838d0d90e
Daniel Rivas <de.rivasherrera@gmail.com>
2023-01-03 23:05:14 -0600

    v2.34 - Dashboard muestra menu base de datos

---
+++ bytebend/api/v1/dashboard/options.php
@@ -0,0 +1,50 @@
+<?php
+
+	//cabezeras para acceso
+	header('Access-Control-Allow-Origin: *');
+	header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method");
+	header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
+	header("Allow: GET, POST, OPTIONS, PUT, DELETE");
+
+	$method = $_SERVER['REQUEST_METHOD'];
+	if ($method == "OPTIONS") {
+		die();
+	}
+
+
+	//conexión a base de datos php
+	include '../data/db.php';
+
+
+	//inicio de la funcion PDO
+	$pdo = new Conexion();
+
+
+	//Listar registros y consultar registro
+	if ($_SERVER['REQUEST_METHOD'] == 'GET') {
+		if (isset($_GET['id'])) {
+			$sql = $pdo->prepare("SELECT * FROM modules WHERE id=:id");
+			$sql->bindValue(':id', $_GET['id']);
+			$sql->execute();
+			$sql->setFetchMode(PDO::FETCH_ASSOC);
+			header('HTTP/1.1 200');
+			header("Content-Type: application/json");
+			echo json_encode($sql->fetchAll());
+			exit;
+		}
+		else {
+			$sql = $pdo->prepare("SELECT * FROM modules");
+			$sql->execute();
+			$sql->setFetchMode(PDO::FETCH_ASSOC);
+			header('HTTP/1.1 200');
+			header("Content-Type: application/json");
+			echo json_encode($sql->fetchAll());
+			exit;
+		}
+	}
+
+	//Si no corresponde a ninguna opción anterior
+	header("HTTP/1.1 400 Bad Request");
+	header("Content-Type: application/json");
+
+?>
---
+++ bytefront/logic/dashboard/logic.js
@@ -0,0 +1,50 @@
+// Función para obtener fecha y hora desde una instancia de tipo fecha
+// esta se concatena y se hace un inner a una propiedad HTML
+// la funcion se inicia al iniciar la página y se repite cada segundo para la fecha
+function fechaYHora() {
+
+    var hoy = new Date();
+    var opciones = {
+        weekday: 'long',
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric'
+    };
+    var fecha = hoy.toLocaleString('es-GT', opciones);
+    var hora = hoy.getHours() + ':' + hoy.getMinutes() + ':' + hoy.getSeconds();
+
+    // Se imprime en pantalla con el ID fechaYHora
+    document.getElementById("fechaYHora").innerHTML = fecha + ', ' + hora;
+
+}
+
+// Se inicia la función y se repite cada segundo
+fechaYHora();
+setInterval(fechaYHora, 1000);
+
+
+function getMenu() {
+
+    var url = 'http://localhost/mbyte/bytebend/api/v1/dashboard/options';
+
+    fetch(url)
+        .then(response => response.json())
+        .then(data => mostrarData(data))
+        .catch(error => console.log(error));
+
+    const mostrarData = (data) => {
+
+        let body = '';
+
+        for (var i = 0; i < data.length; i++) {
+            var j = 0;
+            body += `<div class="col">
+                        ${data[i].rutaModulo}
+                     </div>`;
+        }
+
+        document.getElementById('menu').innerHTML = body;
+        
+    }
+}
+getMenu();
